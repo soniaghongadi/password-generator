28ee6c8fdda67f8d3bd948e1800439fc88b7c682
Sonia Ghongadi <sghongadi@gmail.com>
2020-12-19 20:54:48 -0600

    changes in test script

--- password_generator/test.py
+++ password_generator/test.py
@@ -3,18 +3,18 @@ Test Cases for random password generator
 """
 import unittest
 import sys
-from easy_password_generator import PasswordGenerator
+from easy_password_generator import PassGen
 
 class TestRPG(unittest.TestCase):
 
     def test_generate(self):
         """Test generate() without specifying any attribute"""
-        pg = PasswordGenerator()
+        pg = PassGen()
         self.assertTrue(6 <= len(pg.generate()) <= 16)
 
     def test_generate_with_different_length(self):
         """Test generate() for fixed length"""
-        pg = PasswordGenerator()
+        pg = PassGen()
         length = 16
         pg.minlen = length
         pg.maxlen = length
@@ -22,7 +22,7 @@ class TestRPG(unittest.TestCase):
     
     def test_exclude_chars(self):
         """Test generate() for excluding chars"""
-        pg = PasswordGenerator()
+        pg = PassGen()
         pg.excludeuchars="A"
         self.assertNotIn("A",pg.generate())
 
