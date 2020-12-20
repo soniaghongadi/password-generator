a07e562e8815e35c758ce4ab35d0408f38b6b7b5
Sonia Ghongadi <sghongadi@gmail.com>
2020-12-19 20:10:44 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easy-password-generator",
+    version="0.0.2",
+    author="Sonia Ghongadi",
+    author_email="author@example.com",
+    description="A powerful but simple to use strong password generation library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/soniaghongadi/password-generator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
