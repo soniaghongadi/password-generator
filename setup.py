7a3fcec4934f56510423e1ceac830a886a685abe
Sonia Ghongadi <sghongadi@gmail.com>
2020-12-19 21:37:42 -0600

    Bump up version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="easy-password-generator",
-    version="0.0.5",
+    version="0.0.6",
     author="Sonia Ghongadi",
     author_email="author@example.com",
     description="A powerful but simple to use strong password generation library",
